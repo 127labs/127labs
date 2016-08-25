0254f0e542b3c939465984673eb8eff9ab58a394
Imran Ismail <imran.codely@gmail.com>
2016-08-25 17:56:45 +0800

    Add animation to header

--- src/modules/app/components/Header.js
+++ src/modules/app/components/Header.js
@@ -1,20 +1,138 @@
 import React from 'react'
+import { Motion, spring } from 'react-motion'
 
-function Header ({ bgImg, children }) {
-  return (
-    <header className='header'>
-      <img src={require('assets/images/parallax-header-layer-0.svg')} className='header__parallax-layer header__parallax-layer--0'/>
-      <img src={require('assets/images/parallax-header-layer-1.svg')} className='header__parallax-layer header__parallax-layer--1'/>
-      <img src={require('assets/images/parallax-header-layer-2.svg')} className='header__parallax-layer header__parallax-layer--2'/>
-      <div className='hero hero--dark header__hero section'>
-        <img src={require('assets/images/logo.svg')} alt='127 Labs' className='header__logo' />
-        { children }
-      </div>
-      <img src={require('assets/images/caret-down.svg')} alt='Caret Down' className='header__caret-down' />
-      <div className='header__slant-left' />
-      <div className='header__slant-right' />
-    </header>
-  )
+class Header extends React.Component {
+  state = {
+    layers: [
+      {
+        image: require('assets/images/parallax-header-layer-0.svg'),
+        style: {
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        },
+        step: 15
+      },
+      {
+        image: require('assets/images/parallax-header-layer-1.svg'),
+        style: {
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        },
+        step: 10
+      },
+      {
+        image: require('assets/images/parallax-header-layer-2.svg'),
+        style: {
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        },
+        step: 5
+      }
+    ]
+  }
+
+  handleMouseMove ({ currentTarget: target, pageY: y, pageX: x }) {
+    const width = target.clientWidth / 2
+    const height = target.clientHeight / 2
+
+    this.setState((state, props) => {
+      return {
+        layers: state.layers.map((layer) => ({
+          ...layer,
+          style: this.layerStyleReducer({
+            layer,
+            height,
+            width,
+            x,
+            y
+          })
+        }))
+      }
+    })
+  }
+
+  layerStyleReducer ({ layer, width, height, x, y }) {
+    return Object.keys(layer.style).reduce((acc, key) => {
+      switch (key) {
+        case 'top':
+          if (y < height) {
+            acc.top = layer.step
+          } else {
+            acc.top = 0
+          }
+          return acc
+
+        case 'bottom':
+          if (y > height) {
+            acc.bottom = layer.step
+          } else {
+            acc.bottom = 0
+          }
+          return acc
+
+        case 'left':
+          if (x < width) {
+            acc.left = layer.step
+          } else {
+            acc.left = 0
+          }
+          return acc
+
+        case 'right':
+          if (x > width) {
+            acc.right = layer.step
+          } else {
+            acc.right = 0
+          }
+          return acc
+      }
+    }, {...layer.style})
+  }
+
+  getStyle (index) {
+    return Object.keys(this.state.layers[index].style).reduce((acc, key) => {
+      acc[key] = spring(this.state.layers[index].style[key], [150, 15])
+      return acc
+    }, {})
+  }
+
+  render () {
+    return (
+      <header className='header' onMouseMove={(e) => this.handleMouseMove(e)}>
+        {
+          this.state.layers.map((layer, index) =>
+            <Motion
+              key={index}
+              defaultStyle={layer.style}
+              style={this.getStyle(index)}
+            >
+              {
+                (interpolatedStyle) =>
+                  <img
+                    src={layer.image}
+                    className='header__parallax-layer'
+                    style={interpolatedStyle}
+                  />
+              }
+            </Motion>
+          )
+        }
+        <div className='hero hero--dark header__hero section'>
+          <img src={require('assets/images/logo.svg')} alt='127 Labs' className='header__logo' />
+          { this.props.children }
+        </div>
+        <img src={require('assets/images/caret-down.svg')} alt='Caret Down' className='header__caret-down' />
+        <div className='header__slant-left' />
+        <div className='header__slant-right' />
+      </header>
+    )
+  }
 }
 
 export default Header
