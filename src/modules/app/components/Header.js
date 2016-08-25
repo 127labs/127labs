d440c311a9f9f4a35d9d340df2a54fa297c8949c
Imran Ismail <imran.codely@gmail.com>
2016-08-25 13:30:36 +0800

    [WIP] Parallax layers

--- src/modules/app/components/Header.js
+++ src/modules/app/components/Header.js
@@ -3,6 +3,9 @@ import React from 'react'
 function Header ({ bgImg, children }) {
   return (
     <header className='header'>
+      <img src={require('assets/images/parallax-header-layer-0.svg')} className='header__parallax-layer header__parallax-layer--0'/>
+      <img src={require('assets/images/parallax-header-layer-1.svg')} className='header__parallax-layer header__parallax-layer--1'/>
+      <img src={require('assets/images/parallax-header-layer-2.svg')} className='header__parallax-layer header__parallax-layer--2'/>
       <div className='hero hero--dark header__hero section'>
         <img src={require('assets/images/logo.svg')} alt='127 Labs' className='header__logo' />
         { children }
