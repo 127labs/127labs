c632cc1f81ce24de6430df98dc74bf00d921a5c9
Imran Ismail <imran.codely@gmail.com>
2016-08-27 01:03:49 +0800

    Update tracking code

--- src/config/initialize.js
+++ src/config/initialize.js
@@ -7,11 +7,16 @@ export default (Component) =>
   function initialize () {
     const store = configureStore({})
     const history = syncHistoryWithStore(browserHistory, store)
+    window.ga('create', 'UA-56128900-3', 'auto')
 
     history.listen(function (location) {
-      window.ga('send', 'pageview', location.pathname)
+      const page = `${location.pathname}${location.search}`
+
+      window.ga('set', 'page', page)
+      window.ga('send', 'pageview')
+
       if (process.env.NODE_ENV !== 'production') {
-        console.log('[GA] Sent Page View', location.pathname)
+        console.log('[GA] Sent Page View', page)
       }
     })
 
