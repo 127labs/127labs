b02e86245b373ce8d0e94f3f2ff5cb1b75881d81
Imran Ismail <imran.codely@gmail.com>
2016-08-27 00:52:44 +0800

    Remove react-ga dependency

--- src/config/initialize.js
+++ src/config/initialize.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactGA from 'react-ga'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import configureStore from 'config/configure-store'
@@ -8,15 +7,13 @@ export default (Component) =>
   function initialize () {
     const store = configureStore({})
     const history = syncHistoryWithStore(browserHistory, store)
-    ReactGA.initialize('UA-56128900-3', { debug: process.env.NODE_ENV !== 'production' })
 
     return (
       <Component
         store={store}
         history={history}
         sendToAnalytics={() => {
-          ReactGA.set({page: window.location.pathname})
-          ReactGA.pageview(window.location.pathname)
+          window.ga('send', 'pageview', window.location.pathname)
         }}
       />
     )
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ const config = require('hjs-webpack')({
   html (context) {
     return {
       'index.html': context.defaultTemplate({
+        html: `<div id="root"></div>
+               <script>
+                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                 ga('create', 'UA-56128900-3', 'auto');
+                 ga('send', 'pageview');
+               </script>`,
         head: `<link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
                <link rel="icon" type="image/png" href="/assets/favicon-32x32.png" sizes="32x32">
                <link rel="icon" type="image/png" href="/assets/favicon-16x16.png" sizes="16x16">
